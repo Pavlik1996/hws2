cdc76daf448726701db89731d0f5c007ddb76e1e
Pavel Khrysto <miki2012miki@gmail.com>
2023-02-08 23:07:24 +0300

    HW 10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,10 @@
 import s from './Loader.module.css'
+import loading from '../Dual Ring-1.7s-200px.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+    return (
+        <div className={s.loader}>
+            <img src={loading} alt={'wait'}/>
+        </div>
+    )
+}
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,13 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): { isLoading: boolean } => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading}
 
+        }
         default:
             return state
     }
@@ -17,6 +20,7 @@ type LoadingActionType = {
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+        type: 'CHANGE_LOADING',
+        isLoading,
+    } as const
+)
